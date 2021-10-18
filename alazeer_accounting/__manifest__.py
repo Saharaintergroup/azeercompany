b75210adb825783dc1af86463c0949ec130e0101
Your Name <you@example.com>
2021-10-18 02:02:31 +0200

    [Add]Alazeer

---
+++ alazeer_accounting/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Alazeer Accounting",
+
+    'depends': ['base','account'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/account_invoice_report.xml',
+
+    ],
+
+}
