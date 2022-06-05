6113cf989da34bdfe9f173d37be0d8e27e546d2e
Your Name <you@example.com>
2022-06-05 14:51:27 +0200

    [MOD]expiration date

---
+++ alazeer_inventory/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Alazeer Inventory",
+
+    'depends': ['base','stock','product_expiry'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/product_barcode.xml',
+
+    ],
+
+}
