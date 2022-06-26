f50547b6520e55c826ca0e8a98d866a44f85c94b
Your Name <you@example.com>
2022-06-26 02:56:36 +0200

    [mod]work_center

--- alazeer_inventory/__manifest__.py
+++ alazeer_inventory/__manifest__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Alazeer Inventory ",
+    'name': "Alazeer Inventory",
 
-    'depends': ['base','stock','product_expiry'],
+    'depends': ['base','stock','product_expiry','stock_barcode'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/product_barcode.xml',
+        'views/stock_move_line.xml',
 
     ],
 
